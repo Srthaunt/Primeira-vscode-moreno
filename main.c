170e5d78c7ec14d2e752fd65e7e95f75773bbebb
Srthaunt <thaunt91@gmail.com>
2026-02-19 11:34:26 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,48 +1,27 @@
+#include <time.h>
+#include <stdlib.h> 
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "driver/gpio.h"
-    
-     #define PINO 2
 
-      void pisca_led(int tempo, int vezes,int pino){
-      gpio_set_direction(14,GPIO_MODE_OUTPUT);
-      gpio_set_direction(pino,GPIO_MODE_OUTPUT);
-for (int i =0; i < vezes; i++)
-{
-    gpio_set_level(pino,1);
-    gpio_set_level(14,1);
-    vTaskDelay(tempo / portTICK_PERIOD_MS);
-    gpio_set_level(pino,0);
-    gpio_set_level(14,0);
-    
-        vTaskDelay(100 / portTICK_PERIOD_MS);
-    }
-    }
-    
-    void s(){
-    pisca_led(100,3, 14);
-    pisca_led(100,0, 14);
-    pisca_led(100,3, PINO);
+  
 
-    }
-    
-    void o(){
-    pisca_led(500,3, 14);
-    
-    }
-    void app_main(void)
-    {
-    
-     while(true){
-    s();
-    vTaskDelay(100/portTICK_PERIOD_MS);
-    o();
-    vTaskDelay(100/portTICK_PERIOD_MS);
-    s();
-    printf ("joão quer ajuda\n");
-    }
-    }
+ void app_main() {
+  gpio_set_direction(2,GPIO_MODE_OUTPUT);
+srand((unsigned int) time(NULL));
+  int min = 30;
+  int max = 50;
+while (true){
+  int temp = (rand() % (max - min + 1)) +min;
+ printf(" sua temperatura é %d\n" ,temp);
+ if (  temp < 40){
+ 
+   vTaskDelay(3000 / portTICK_PERIOD_MS);
+ 
    
-   
-    
+  if ( temp > 40)
+  gpio_set_level(2,1);
+ }
+}
+ }
